b970fa02834b79e19e1fcb27c80644f81015b57e
Umbree <evan.m.jun@gmail.com>
2026-07-07 08:36:12 -0400

    Small test for ticketcategory thingy I should really work on

--- main/modules/admin/commands.lua
+++ main/modules/admin/commands.lua
@@ -9,15 +9,30 @@ commands["addticket"] = {
 }
 commands["markticketcategory"] = {
 	cmd = "markticketcategory",
-	helper = "markTicketCategory -- Marks category as a ticket category to auto generate text channels when tickets come in.",
+	helper = "markTicketCategory [name] -- Marks category as a ticket category to auto generate text channels when tickets come in.",
 	perms = {"manageChannels"},
 	func = function(message, args)
-		BOT_STORAGE[message.guild.id] = BOT_STORAGE[message.guild.id] or {}
-		BOT_STORAGE[message.guild.id].ticketCategory = BOT_STORAGE[message.guild.id].ticketCategory or {}
-		BOT_STORAGE[message.guild.id].ticketCategory = { channelId = message.channel.id, messageId = sent.id }
-		message:replydel("Marked category as a ticket category!")
-		saveData()
-		message:delete()
+		local arg = args[2]
+		if (not arg) or (arg == "") then 
+			message:replydel("Usage: `"..BOT_PREFIX.."markticketcategory` `[category name]`\n-# Not case sensitive. If the category has spaces sorround it with quotation marks!")
+			util:addToDelete(message)
+			return
+		end
+		arg = arg:lower()
+		local category = nil
+		for id, obj in pairs(message.guild.categories) do if obj.name:lower() == arg then category = obj break end end
+		if not category then
+			message:replydel("Couldn't find the listed category, apologies.")
+			util:addToDelete(message)
+			return
+		else
+			print(category)
+			-- BOT_STORAGE[message.guild.id] = BOT_STORAGE[message.guild.id] or {}
+			-- BOT_STORAGE[message.guild.id].ticketCategory = nil
+			-- saveData()
+			message:replydel("This is where I would do something. If I had an idea.")
+			util:addToDelete(message)
+		end
 	end
 }
 return commands
